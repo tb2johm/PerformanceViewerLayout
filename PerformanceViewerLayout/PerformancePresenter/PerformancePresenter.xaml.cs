ed1a63e1571d7761cf5285a5718e4385ce686043
tb2johm <tb2johm@users.noreply.github.com>
2016-11-24 15:07:33 +0100

    Adding code base

---
+++ PerformanceViewerLayout/PerformancePresenter/PerformancePresenter.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PerformanceViewerLayout.PerformancePresenter
+{
+    /// <summary>
+    /// Interaction logic for PerformancePresenter.xaml
+    /// </summary>
+    public partial class PerformancePresenter : UserControl
+    {
+        public PerformancePresenter()
+        {
+            InitializeComponent();
+        }
+    }
+}
